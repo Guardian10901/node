b20a4efb5e6f05c074fba92f7cffdca330ee8463
Vidya V <vidyav10901@gmail.com>
2023-08-02 16:58:27 +0530

    middleware orm implemented

--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
+import "reflect-metadata";
 import express from "express";
 import { Employee } from "./employee";
 import employeeRouter from "./employee_router";
 import loggerMiddleware from "./loggerMIddleware";
+import dataSourse from "./data-sourse";
+
+
 const server = express();
 server.use(express.json());
 server.use(loggerMiddleware);
 server.use('/employees', employeeRouter);
 
-server.listen(3000, () => {
+(async () => {
+    await dataSourse.initialize();
+    server.listen(3000, () => {
     console.log("server is listening to 3000")
-});
+    });
+})();
+
+
--- employee_router.ts
+++ employee_router.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import client from "pg"
 import { Employee } from "./employee";
+
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { DataSource } from "typeorm";
+import dataSourse from "./data-sourse";
 const employeeRouter = express.Router();
 let count = 2;
 const employee: Employee[] = [{
@@ -15,48 +20,58 @@ const employee: Employee[] = [{
     createdAt: new Date(),
     updatedAt: new Date()
 }]
-employeeRouter.get('/', (req, res) => {
+employeeRouter.get('/',async (req, res) => {
     console.log(req.url);
+    const employeeRepository = dataSourse.getRepository(Employee);
+    const employee = await employeeRepository.find();
     res.status(200).send(employee);
 })
-employeeRouter.get('/:id', (req, res) => {
+employeeRouter.get('/:id', async (req, res) => {
     console.log(req.url);
-    const id = Number(req.params.id);
+    
+    
+    const employeeRepository = dataSourse.getRepository(Employee);
+    const employee = await employeeRepository.findOneBy({
+        id: Number(req.params.id),
+    });
+    res.status(200).send(employee);
 
-    const emp = employee.find(element => element.id == id)
-    res.status(200).send(emp);
 })
-employeeRouter.post('/', (req, res) => {
+employeeRouter.post('/', async(req, res) => {
     console.log(req.url);
     const newemployee = new Employee();
-    newemployee.id= ++count;
+    newemployee.id = ++count;
     newemployee.name = req.body.name;
     newemployee.email = req.body.email;
     newemployee.createdAt = new Date();
     newemployee.updatedAt = new Date();
+
+    const employeeRepository = dataSourse.getRepository(Employee);
+    const employee = await employeeRepository.save(newemployee);
+
+    res.status(201).send(newemployee);
+})
+employeeRouter.put('/:id', async(req, res) => {
+    console.log(req.url);
     
-    employee.push(newemployee);
+    const employeeRepository = dataSourse.getRepository(Employee);
+    const employee = await employeeRepository.findOneBy({
+        id: Number(req.params.id),
+    });
 
-        res.status(201).send(newemployee);
+    employee.name=req.body.name;
+    employee.email=req.body.email;
+    await employeeRepository.save(employee);
+
+    res.status(200).send();
 })
-employeeRouter.put('/:id', (req, res) => {
+employeeRouter.delete('/:id', async (req, res) => {
     console.log(req.url);
-    const id = Number(req.params.id);
-
-    const emp = employee.find(element => element.id == id)
-    emp.name=req.body.name;
-    emp.email=req.body.email;
-    emp.updatedAt=new Date();
-
-
-    res.status(200).send(emp);
-})
-employeeRouter.delete('/:id', (req, res) => {
-    console.log(req.url);
-    const id = Number(req.params.id);
-
-    const emp = employee.findIndex(element => element.id == id)
-    employee.splice(emp,1);
-    res.status(204);
+    const employeeRepository = dataSourse.getRepository(Employee);
+    const employee = await employeeRepository.findOneBy({
+        id: Number(req.params.id),
+    });
+    await employeeRepository.remove(employee);
+    res.status(204).send();
 })
 export default employeeRouter;
